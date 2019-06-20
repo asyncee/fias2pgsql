ff9755813cfbc6c75544e1b498ed5271eaae173d
Egor Urvanov <hedgehogues@bk.ru>
2019-06-20 23:05:22 +0300

    add docker-compose is added

--- index.sh
+++ index.sh
@@ -1,10 +1,13 @@
+#!/usr/bin/env bash
 echo '_______________________НАЧИНАЮ ИМПОРТ ИЗ ФАЙЛОВ__________________'
+echo $POSTGRES_DB
+echo $POSTGRES_USER
 bash import.sh
 
 echo '_______________________НАЧИНАЮ ОБНОВЛЕНИЕ СХЕМЫ__________________'
-psql postgresql://$USER:$PASSWORD@$HOST:$PORT/$DB -f update_schema.sql
+psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -f update_schema.sql
 
 echo '_______________________НАЧИНАЮ СОЗДАНИЕ ИНДЕКСОВ_________________'
-psql postgresql://$USER:$PASSWORD@$HOST:$PORT/$DB -f indexes.sql
+psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -f indexes.sql
 
 echo '_______________________ _________ГОТОВО________ _________________'
