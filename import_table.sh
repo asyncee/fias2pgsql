8f5e79a1687745672b2c3ef9ffbb692cefe33d68
Konstantin Kostikov <konstantin.kostikov@teradata.com>
2019-07-16 15:30:16 +0300

    multipurpose load table script bugfix

--- import_table.sh
+++ import_table.sh
@@ -14,18 +14,19 @@ do
     TABLE="${FILE%.*}"
 
     pgdbf $FULLPATH | iconv -f cp866 -t utf-8 | psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB
-    echo "++++++++++++++++++ TABLE $TABLE CREATED"
+    echo "++++++++++++++++++ TEMP TABLE $TABLE LOADED"
 
     if [[ "$first_loop_flag" = true ]]; then
-      psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -c "CREATE TABLE IF NOT EXISTS $TBL_TO_LOAD  AS SELECT * FROM $TABLE WHERE 0 = 1"
-      echo "++++++++++++++++++ TABLE $TBL_TO_LOAD CREATED"
+      psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -c "CREATE TABLE IF NOT EXISTS $TBL_TO_LOAD  AS SELECT * FROM $TABLE WHERE 0 = 1;"
+      echo "++++++++++++++++++ TARGET TABLE $TBL_TO_LOAD CREATED"
       first_loop_flag=false
     fi
 
     echo "++++++++++++++++++ INSERT $TABLE DATA INTO $TBL_TO_LOAD"
     psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -c "INSERT INTO $TBL_TO_LOAD SELECT * FROM $TABLE;"
 
-    if [[ "$TABLE" -ne "$TBL_TO_LOAD" ]]; then
+    if [[ "$TABLE" != "$TBL_TO_LOAD" ]]; then
+        echo "++++++++++++++++++ DROP TABLE $TABLE"
         psql postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB -c "DROP TABLE $TABLE;"
     fi
 
